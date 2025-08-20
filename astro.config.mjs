6bab8cd5a49d5ff299fa9500b71fa3b02f93ba03
thehunter101 <thehuntyer10121@gmail.com>
2025-08-19 19:40:32 -0500

    landing 100

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 site:'https://alexander565-2.github.io',
-base: 'Fragancia_Deluxe_Astro',
+base: '/Fragancia_Deluxe_Astro/',
  outDir: 'docs',
 });
